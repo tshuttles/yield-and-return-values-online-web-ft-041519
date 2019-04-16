f2e75dca86f5962a871df462f67521a180edb972
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-16 01:55:57 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,10 +4,12 @@ def hello(array)
   i = 0
   collection = [] 
   while i < array.length
+  binding.pry
     collection << yield(array[i])
     i += 1
   end
   collection 
 end
 
+binding.pry 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
